e335be4140e72659b57a83de3b4899b5d57c2f6f
TheFoxyOne <t0ny@hotmail.gr>
2022-05-02 22:21:38 +0300

    More Mobile Styling.

---
+++ dist/pages/submit_page/submit_exhibitor.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- HEADER -->
+
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/dist/global/components/head.php'; ?>
+    <link rel="stylesheet" href="../../css/submit_exhibitor.css">
+</head>
+
+<!-- BODY -->
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/dist/pages/home_page/components/header.php'; ?>
+    
+    <div class="answer_container">
+        <h1 id="emailAnswer"></h1>
+    </div>
+    
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/dist/pages/home_page/components/footer.php'; ?>
+    
+</body>
+
+</html>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/dist/pages/submit_page/helpers/submit_exhibitor_helper.php'; ?>
